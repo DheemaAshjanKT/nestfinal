70036712c2e8f9a703d42fee425a3a87bb8c3c42
Dheema Ashjan KT <dheemaashjankt@gmail.com>
2024-10-25 22:01:57 +0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductComponent } from './product/product.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component:LoginComponent
+    },
+{
+    path :'home',component: HomeComponent
+},
+{
+    path: 'about', component: AboutComponent
+},
+{
+    path: 'product/:id', component: ProductComponent
+}
+];
